b2d527c66bb8601737c93b153fbc383898d96dfc
Leo <102501213+US-USA@users.noreply.github.com>
2026-04-15 14:41:51 +0200

    Add profile update functionality using API

---
+++ update_profile.py
@@ -0,0 +1,22 @@
+import cloudscraper, json
+
+requests = cloudscraper.create_scraper()
+
+url = "https://truthsocial.com/api/v1/accounts/update_credentials"
+
+payload = {
+  'display_name': "Welcome", # Name
+  'note': "This is Bio", # Bio
+  'website': "t.me/z_0_g", # Url
+  'location': "" # Location 
+}
+
+headers = {
+  'User-Agent': "TruthSocialAndroid/okhttp/5.1.0/1230",
+  'authorization': "Bearer idk**", # Token
+  'accept-language': "en-US"
+}
+
+response = requests.patch(url, data=payload, headers=headers)
+
+print(json.dumps(response.json(), indent=2, ensure_ascii=False))
